59f01eb02f1a5adeb6be8c594ab597841ff4d692
Jonathan Horovitz <jonathan.horovitz@gmail.com>
2024-11-18 18:28:47 +0200

    fixes for some param_sweep_2 configs.

--- check_results.sh
+++ check_results.sh
@@ -1,7 +1,9 @@
-for file in output_bak/*; do
-    grep model $file
-    grep learning_rate $file
-    grep Parameters $file
+for file in output/*; do
+    echo $file
+    grep \"name $file
+    grep \"model $file
+    grep \"learning_rate $file
+    grep ^Parameters $file
     grep MAE $file | tail -n 1
     echo "================="
 done
